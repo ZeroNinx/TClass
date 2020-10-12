79257a39be2045b547ba76d4228c5bf70b634d01
ZeroNinx <zwq93968137@sina.com>
2020-10-12 16:17:11 +0800

    初始commit

---
+++ Source/TClass/TClass.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TClass.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TClass, "TClass" );
