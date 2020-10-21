28f8bd8dfbc5980180cab9863239160aa8415413
ZeroNinx <zwq93968137@sina.com>
2020-10-21 20:41:47 +0800

    VS:迁移GameInstance到C++

---
+++ Source/TClass/GM_Menu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Menu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCLASS_API AGM_Menu : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TClass/UMG_MainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UMG_MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCLASS_API UUMG_MainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
