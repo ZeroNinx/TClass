ce0d5b9714080ddb9d95d49079b6ab7c43ce6bda
ZeroNinx <zwq93968137@sina.com>
2020-11-02 21:28:38 +0800

    初步完成单人游戏

---
+++ Source/TClass/TPSPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TPSPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCLASS_API ATPSPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
