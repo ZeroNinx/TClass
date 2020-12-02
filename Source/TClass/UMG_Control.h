2d74121bd558f8c803ccabb015d178c3146933d7
ZeroNinx <zwq93968137@sina.com>
2020-12-02 23:38:44 +0800

    部分迁移至C++

---
+++ Source/TClass/UMG_Control.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UMG_Control.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCLASS_API UUMG_Control : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TClass/UMG_TPS.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UMG_TPS.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCLASS_API UUMG_TPS : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
